963125309acbb37b4bb0e1082077e458a99ef94f
XLazer <khannarahul446@gmail.com>
2023-07-20 20:17:35 +0530

    made abi external

--- frontend/src/FileUploader.js
+++ frontend/src/FileUploader.js
@@ -1,107 +1,10 @@
 import React, { useState } from "react";
 import { ethers } from "ethers";
+import ABI from "./ContractABI.json";
 
-// Assuming the contract is already instantiated and connected to the right network
-// let's call this contract instance as `contract`
 const provider = new ethers.providers.JsonRpcProvider(
   "https://rpc.ankr.com/polygon_mumbai"
 );
-const ABI = [
-  {
-    anonymous: false,
-    inputs: [
-      {
-        indexed: true,
-        internalType: "address",
-        name: "previousOwner",
-        type: "address",
-      },
-      {
-        indexed: true,
-        internalType: "address",
-        name: "newOwner",
-        type: "address",
-      },
-    ],
-    name: "OwnershipTransferred",
-    type: "event",
-  },
-  {
-    inputs: [],
-    name: "renounceOwnership",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "uint256",
-        name: "documentId",
-        type: "uint256",
-      },
-      {
-        internalType: "bytes32",
-        name: "documentHash",
-        type: "bytes32",
-      },
-    ],
-    name: "storeHash",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "address",
-        name: "newOwner",
-        type: "address",
-      },
-    ],
-    name: "transferOwnership",
-    outputs: [],
-    stateMutability: "nonpayable",
-    type: "function",
-  },
-  {
-    inputs: [],
-    name: "owner",
-    outputs: [
-      {
-        internalType: "address",
-        name: "",
-        type: "address",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-  {
-    inputs: [
-      {
-        internalType: "uint256",
-        name: "documentId",
-        type: "uint256",
-      },
-      {
-        internalType: "bytes32",
-        name: "documentHash",
-        type: "bytes32",
-      },
-    ],
-    name: "verifyHash",
-    outputs: [
-      {
-        internalType: "bool",
-        name: "",
-        type: "bool",
-      },
-    ],
-    stateMutability: "view",
-    type: "function",
-  },
-];
 const contract = new ethers.Contract(
   "0x6a11a7f744c733fd6d8c927947aad0b25745e327",
   ABI,
