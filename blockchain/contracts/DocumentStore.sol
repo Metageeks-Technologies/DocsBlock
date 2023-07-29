0b1faf19a328a701e44fce57d311963eb57ce5a0
XLazer <khannarahul446@gmail.com>
2023-07-29 16:49:06 +0530

    added gelato relayer support

--- blockchain/contracts/DocumentStore.sol
+++ blockchain/contracts/DocumentStore.sol
@@ -1,14 +1,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-import "@gelatonetwork/relay-context/contracts/vendor/ERC2771Context.sol";
+import {ERC2771Context} from "@gelatonetwork/relay-context/contracts/vendor/ERC2771Context.sol";
 
 contract DocumentStore is ERC2771Context {
     mapping(bytes32 => bytes32) private documentHashes;
 
-    constructor(address _trustedForwarder) ERC2771Context(_trustedForwarder) {}
+    constructor()
+        ERC2771Context(address(0x97015cD4C3d456997DD1C40e2a18c79108FCc412))
+    {}
 
-    function storeHash(bytes32 documentId, bytes32 documentHash) public {
+    modifier onlyTrustedForwarder() {
+        require(
+            isTrustedForwarder(msg.sender),
+            "Only callable by Trusted Forwarder"
+        );
+        _;
+    }
+
+    function storeHash(
+        bytes32 documentId,
+        bytes32 documentHash
+    ) external onlyTrustedForwarder {
         require(documentHashes[documentId] == 0, "Document hash already set");
         documentHashes[documentId] = documentHash;
     }
@@ -16,7 +29,7 @@ contract DocumentStore is ERC2771Context {
     function verifyHash(
         bytes32 documentId,
         bytes32 documentHash
-    ) public view returns (bool) {
+    ) external view returns (bool) {
         return documentHashes[documentId] == documentHash;
     }
 }
